b8d64850b706b6f26d91667265980c8ceb2cc764
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-02-20 19:27:59 +0530

    updated sponsor content

--- app/_components/sponsor.tsx
+++ app/_components/sponsor.tsx
@@ -3,10 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import DEVFOLIO from "@/public/sponsors/devfolio.png";
-import POLYGON from "@/public/sponsors/polygon.png";
-import REPLIT from "@/public/sponsors/replit.png";
-import SOLANA from "@/public/sponsors/solana.png";
-import FILECOIN from "@/public/sponsors/filecoin.png";
+import MULEARN from "@/public/sponsors/mulearn.png";
+import FUNDESIGN from "@/public/sponsors/fundesign.png";
 
 const Sponsors = () => {
   const sponsors = [
@@ -14,6 +12,16 @@ const Sponsors = () => {
       logo: DEVFOLIO,
       url: "https://devfolio.co/",
       alt: "DEVFOLIO LOGO",
+    },
+    {
+      logo: FUNDESIGN,
+      url: "https://fundesign.com/",
+      alt: "FUNDESIGN LOGO",
+    },
+    {
+      logo: MULEARN,
+      url: "https://mulearn.io/",
+      alt: "MULEARN LOGO",
     },
     // {
     //   logo: POLYGON,
@@ -36,39 +44,18 @@ const Sponsors = () => {
     //   alt: "FILECOIN LOGO",
     // },
   ];
-  // return (
-  //   <div className="z-10 flex flex-col items-center justify-center font-secondary">
-  //     <div className="text-3xl text-white sm:text-6xl">
-  //       <span className="text-tertiary">partnering</span> with
-  //     </div>
-  //     <div className="mt-4 flex flex-wrap items-center justify-center gap-8 py-7 sm:gap-16">
-  //       {sponsors.map((sponsor) => (
-  //         <Link key={sponsor.url} href={sponsor.url} className="cursor-pointer">
-  //           <Image
-  //             src={sponsor.logo}
-  //             className="opacity-50 transition duration-200 ease-in hover:opacity-100"
-  //             height={33}
-  //             onClick={() => window.open(sponsor.url)}
-  //             alt={sponsor.alt}
-  //           />
-  //         </Link>
-  //       ))}
-  //     </div>
-  //   </div>
-  // );
-
   return (
-    <div className="z-10 my-0 flex flex-col items-center justify-center gap-0 font-secondary sm:-my-6 md:-my-12 lg:-my-16 xl:gap-2">
-      <div className="text-center text-3xl tracking-wider text-white sm:text-4xl md:text-5xl lg:text-6xl">
+    <div className="z-10 flex flex-col items-center justify-center font-secondary">
+      <div className="text-3xl text-white sm:text-6xl">
         <span className="text-tertiary">partnering</span> with
       </div>
-      <div className="flex flex-wrap items-center justify-center gap-8 py-7 sm:gap-16">
+      <div className="mt-4 flex flex-wrap items-center justify-center gap-8 py-7 sm:gap-16">
         {sponsors.map((sponsor) => (
           <Link key={sponsor.url} href={sponsor.url} className="cursor-pointer">
             <Image
               src={sponsor.logo}
-              className="sm:scale-120 opacity-50 transition duration-200 ease-in hover:opacity-100 lg:scale-150"
-              height={28}
+              className="opacity-50 transition duration-200 ease-in hover:opacity-100"
+              height={33}
               onClick={() => window.open(sponsor.url)}
               alt={sponsor.alt}
             />
@@ -78,5 +65,26 @@ const Sponsors = () => {
     </div>
   );
 };
+//   return (
+//     <div className="z-10 my-0 flex flex-col items-center justify-center gap-0 font-secondary sm:-my-6 md:-my-12 lg:-my-16 xl:gap-2">
+//       <div className="text-center text-3xl tracking-wider text-white sm:text-4xl md:text-5xl lg:text-6xl">
+//         <span className="text-tertiary">partnering</span> with
+//       </div>
+//       <div className="flex flex-wrap items-center justify-center gap-8 py-7 sm:gap-16">
+//         {sponsors.map((sponsor) => (
+//           <Link key={sponsor.url} href={sponsor.url} className="cursor-pointer">
+//             <Image
+//               src={sponsor.logo}
+//               className="sm:scale-120 opacity-50 transition duration-200 ease-in hover:opacity-100 lg:scale-150"
+//               height={28}
+//               onClick={() => window.open(sponsor.url)}
+//               alt={sponsor.alt}
+//             />
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Sponsors;
