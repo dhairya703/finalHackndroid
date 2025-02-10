68621fb447dfdc9d49ab4a0d938327148ba0e241
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-02-10 14:22:46 +0530

    added domains

--- app/_components/problemStatements.tsx
+++ app/_components/problemStatements.tsx
@@ -1,11 +1,17 @@
+'use client'
+import HoverTextAnimation from "@/components/cards/HoverAnimation";
 import { CustomText } from "@/components/custom";
 import Link from "next/link";
 
 const ProblemStatements = () => {
   const problemStatements = [
-    { href: "/problem-statement-1", title: "1" },
-    { href: "/problem-statement-2", title: "2" },
-    { href: "/problem-statement-3", title: "3" },
+    { href: "/problem-statement-1", title: "1", hover: "edTech" },
+    { href: "/problem-statement-2", title: "2", hover: "Law" },
+    { href: "/problem-statement-3", title: "3", hover: "Healthcare" },
+    { href: "/problem-statement-3", title: "4", hover: "Public Safety" },
+    { href: "/problem-statement-3", title: "5", hover: "Sustainability" },
+    { href: "/problem-statement-3", title: "6", hover: "Modern Ecommerce Solution" },
+    { href: "/problem-statement-3", title: "7", hover: "Open Innovation" },
   ];
   return (
     <div className="z-20">
@@ -15,15 +21,10 @@ const ProblemStatements = () => {
 
       <div className="flex flex-wrap items-center justify-around gap-10 p-10">
         {problemStatements.map((problem, index) => (
-          <Link
-            key={index}
-            href={problem.href}
-            className="flex size-32 cursor-pointer items-center justify-center rounded-2xl border-4 border-primary font-secondary text-5xl font-bold transition-all duration-100 ease-in hover:border-secondary md:size-40 md:text-6xl"
-          >
-            {problem.title}
-          </Link>
+          <HoverTextAnimation key={index} default={problem.title} hover={problem.hover}/>
         ))}
       </div>
+      
     </div>
   );
 };
---
+++ components/cards/HoverAnimation.tsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const HoverTextAnimation = ({ default: defaultText, hover }: { default: string; hover: string }) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+      <motion.div
+        className={`flex size-32 cursor-pointer text-center items-center justify-center rounded-2xl border-4 border-primary font-secondary ${hovered? "text-xl" : "text-5xl"} font-bold transition-all duration-100 ease-in hover:border-secondary md:size-40 `}
+        onHoverStart={() => setHovered(true)}
+        onHoverEnd={() => setHovered(false)}
+      >
+        <motion.span
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          {hovered ? hover : defaultText}
+        </motion.span>
+      </motion.div>
+  );
+};
+
+export default HoverTextAnimation;
