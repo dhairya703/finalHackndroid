36e7747f0385562e2422eed40702fbe1625d22d4
Govind Sankar <govindsankar2005@gmail.com>
2026-03-03 14:06:19 +0530

    Small comment change

--- src/app/certificate/page.tsx
+++ src/app/certificate/page.tsx
@@ -7,7 +7,7 @@ import CertificateLayout from "@/components/pages/certificate/CertificateLayout"
 export default function CertificatePage() {
     return(
         <div className="min-h-screen flex flex-col bg-black text-white items-center justify-center">
-            You do not have access to this page!
+            {/* You do not have access to this page! */}
             <CertificateLayout />
         </div>
     );
