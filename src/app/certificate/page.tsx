c45d41723f7493a65dcf457df7be38e0186815a5
Govind Sankar <govindsankar2005@gmail.com>
2026-03-03 17:43:59 +0530

    Added Certificates Support

--- src/app/certificate/page.tsx
+++ src/app/certificate/page.tsx
@@ -7,8 +7,8 @@ import CertificateLayout from "@/components/pages/certificate/CertificateLayout"
 export default function CertificatePage() {
     return(
         <div className="min-h-screen flex flex-col bg-black text-white items-center justify-center">
-            You do not have access to this page!
-            {/* <CertificateLayout /> */}
+            {/* You do not have access to this page! */}
+            <CertificateLayout />
         </div>
     );
 };
