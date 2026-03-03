acb02baabf526ecacc497e71d34ec6e3b8b70c35
Govind Sankar <govindsankar2005@gmail.com>
2026-03-03 14:26:52 +0530

    Live leaderboard added

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -7,7 +7,8 @@ import LeaderboardLayout from "@/components/pages/leaderboard/LeaderboardLayout"
 export default function LeaderboardPage() {
     return(
         <div className="min-h-screen flex flex-col bg-black text-white items-center justify-center">
-            You do not have access to this page!
+            {/* You do not have access to this page! */}
+            <LeaderboardLayout />
         </div>
     );
 };
